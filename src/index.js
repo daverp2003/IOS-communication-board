87ca5d95cc94e099e665d3b81a8dc4ec68493cff
daverp2003 <daverp2003@gmail.com>
2026-03-07 10:18:16 -0500

    fix: resolve merge conflicts

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
->>>>>>> 585a3f9eb3a482b74cd3fc42b87c873efeeab7a3
 root.render(
   <React.StrictMode>
     <App />
